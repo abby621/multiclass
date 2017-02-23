0611f9aec76638b4555baf17af09b7b7a976989b
abby <sw62190>
2017-02-23 13:25:38 -0600

    Classify 64 images at a time

--- code/classify_hotel_bathroom_other.py
+++ code/classify_hotel_bathroom_other.py
@@ -18,7 +18,7 @@ with open(im_path,'rU') as f:
 
 origSize = 256
 cropSize = 224
-batchSize = 10
+batchSize = 64
 
 mean_im_path = '/project/focus/abby/hotelnet/models/places205CNN_mean.binaryproto'
 blob = caffe.proto.caffe_pb2.BlobProto()
@@ -38,21 +38,33 @@ transformer.set_channel_swap('data', (2,1,0))
 transformer.set_raw_scale('data', 255.0)
 
 pred = np.empty(len(imList))
-for ix in range(0,len(imList)):
-    try:
-        orig_im = caffe.io.load_image(imList[ix][0])
-        im_oversampled = caffe.io.oversample([caffe.io.resize_image(orig_im, (origSize,origSize))], (cropSize,cropSize))
-        caffe_input = np.asarray([transformer.preprocess('data', crop) for crop in im_oversampled])
-        net.blobs['data'].data[...] = caffe_input
-        out = net.forward()
-        prob = out['prob']
-        pred[ix] = np.argmax(np.mean(prob,0))
-        if pred[ix] == 1:
-            out_file.write(imList[ix][0] + ' ' + imList[ix][1] + '\n')
-            print 'Including ' + imList[ix][0]
+ctr = 0
+numIms = len(imList)
+for ix in range(0,numIms,batchSize):
+    loopSz = min(batchSize,numIms-ix)
+    if loopSz != batchSize:
+        net.blobs['data'].reshape(loopSz,3,cropSize,cropSize)
+        transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+        transformer.set_mean('data', mean_im)
+        transformer.set_transpose('data', (2,0,1))
+        transformer.set_channel_swap('data', (2,1,0))
+        transformer.set_raw_scale('data', 255.0)
+    caffe_input = np.zeros((loopSz,3,cropSize,cropSize))
+    for iy in range(0,loopSz):
+        orig_im = caffe.io.load_image(imList[ctr][0])
+        caffe_input[iy,:,:,:] = transformer.preprocess('data',orig_im)
+        ctr += 1
+    
+    net.blobs['data'].data[...] = caffe_input
+    out = net.forward()
+    prob = out['prob']
+    this_pred = np.argmax(prob,1)
+    pred[ctr-loopSz:ctr] = this_pred
+    for ax in range(ctr-loopSz-1,ctr-1):
+        if pred[ax] == 1:
+            out_file.write(imList[ax][0] + ' ' + imList[ax][1] + '\n')
+            print 'Including ' + imList[ax][0]
         else:
-            print 'Not including ' + imList[ix][0]
-    except:
-        continue
+            print 'Not including ' + imList[ax][0]
 
 out_file.close()
