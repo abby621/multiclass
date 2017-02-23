eeeda3fc7194a86100682fa8e97ff8e1d5eb6960
abby <sw62190>
2017-02-23 14:53:38 -0600

    Code for oversampling classes

---
+++ code/oversample_classes.py
@@ -0,0 +1,39 @@
+import csv
+import numpy as np
+import shutil
+
+old_file_path = 'train.txt'
+old_file = open(old_file_path,'rU')
+reader = csv.reader(old_file,delimiter=' ')
+imList = list(reader)
+
+starRatings = [int(im[1]) for im in imList]
+npStarRatings = np.asarray(starRatings)
+
+oneAndTwoStar = np.where(npStarRatings==0)[0]
+threeStar = np.where(npStarRatings==1)[0]
+fourStar = np.where(npStarRatings==2)[0]
+
+maxNum = np.max((oneAndTwoStar.shape[0],threeStar.shape[0],fourStar.shape[0]))
+
+new_file_path = old_file_path.split('.')[0] + '_oversampled.txt'
+shutil.copy(old_file_path,new_file_path)
+new_file = open(new_file_path,'a')
+
+numOneTwoStar = oneAndTwoStar.shape[0]
+while numOneTwoStar < maxNum:
+    toAdd = imList[random.choice(oneAndTwoStar)]
+    new_file.write('%s %s\n', (toAdd[0], toAdd[1]))
+    numOneTwoStar += 1
+
+numThreeStar = threeStar.shape[0]
+while numThreeStar < maxNum:
+    toAdd = imList[random.choice(threeStar)]
+    new_file.write('%s %s\n', (toAdd[0], toAdd[1]))
+    numThreeStar += 1
+
+numFourStar = fourStar.shape[0]
+while numFourStar < maxNum:
+    toAdd = imList[random.choice(fourStar)]
+    new_file.write('%s %s\n', (toAdd[0], toAdd[1]))
+    numFourStar += 1
